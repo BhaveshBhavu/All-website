d5ae57812a10221e7908d037ec8780af41e35a81
bhaveshabcd1234bhavu@gmail.com <bhaveshabcd1234bhavu@gmail.com>
2022-06-26 15:46:24 +0530

    mn

--- main.js
+++ main.js
@@ -2,18 +2,18 @@ console.log('Hello World!');
 
 
 
-for (var o = 0; o < 18; o++) {
+for (var o = 0; o < 16; o++) {
 
-var img = new Array('https://www.freepnglogos.com/uploads/doraemon-png/doraemon-keren-kumpulan-vector--5.png', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdjOy24GD89HAkvuWWBq3wyJi3pWs9HGqvyKQMjG0d2i4239g-ZuaObj5x&s=10','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWsCqc1FvnqwOJLUQ5XhmNOk7bCALvv7zkGw&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgrKsREEtqBw9dvhdXihlHYCUAiRfxBxpdrA&usqp=CAU', 'https://whatsappstatuszone.com/wp-content/uploads/2021/04/Romantic-love-song-nobita-shizuka-HD-status-%F0%9F%92%AB%F0%9F%92%93-%F0%9F%94%B7%F0%9F%94%B7Cute-Whatsapp-Status-Doremon-status-life%F0%9F%94%B7%F0%9F%94%B7.mp4.Still001-1.jpg','https://i.pinimg.com/736x/a3/55/43/a355438528dc074fddd710a3dae9559d.jpg','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXltevQNZhS9wFgnb_lb3nK8VovW__leLL-Q&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdjOy24GD89HAkvuWWBq3wyJi3pWs9HGqvyKQMjG0d2i4239g-ZuaObj5x&s=10','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWsCqc1FvnqwOJLUQ5XhmNOk7bCALvv7zkGw&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgrKsREEtqBw9dvhdXihlHYCUAiRfxBxpdrA&usqp=CAU', 'https://whatsappstatuszone.com/wp-content/uploads/2021/04/Romantic-love-song-nobita-shizuka-HD-status-%F0%9F%92%AB%F0%9F%92%93-%F0%9F%94%B7%F0%9F%94%B7Cute-Whatsapp-Status-Doremon-status-life%F0%9F%94%B7%F0%9F%94%B7.mp4.Still001-1.jpg','https://i.pinimg.com/736x/a3/55/43/a355438528dc074fddd710a3dae9559d.jpg','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwC69ODZ62TfF7RMU90URX9uqOV591-KsjvtoAlxjbExQhS-AJRdzIkbOQ&s=10','https://i.pinimg.com/736x/8f/3a/63/8f3a6370e072a3624e8b92077c525fd8.jpg','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwQ40aDQgjB2LeW4ynHQvX27tQGkaEwycAXQ&s','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdjOy24GD89HAkvuWWBq3wyJi3pWs9HGqvyKQMjG0d2i4239g-ZuaObj5x&s=10','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWsCqc1FvnqwOJLUQ5XhmNOk7bCALvv7zkGw&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgrKsREEtqBw9dvhdXihlHYCUAiRfxBxpdrA&usqp=CAU');
+var img = new Array('img/1.jpg', 'img/2.jpg','img/3.jpeg', 'img/4.png', 'img/5.jpg','img/6.jpg','img/7.jpeg', 'img/8.jpg','img/9.jpg', 'img/10.jpg', 'img/11.jpg','img/12.jpg','img/13.jpg','img/14.jpg','img/15.jpg','img/16.png');
 
-
+var a = new Array('https://bhaveshbhavu.github.io/Doremon/', 'https://bhaveshbhavu.github.io/password-N/','https://bhaveshbhavu.github.io/LET-S-TRAVEL/', 'https://bhaveshbhavu.github.io/YOU-TUBE/', 'https://bhaveshbhavu.github.io/May-play/','https://bhaveshbhavu.github.io/food/','https://bhaveshbhavu.github.io/Loki/', 'https://bhaveshbhavu.github.io/img/','https://bhaveshbhavu.github.io/HDJum.Com/', 'https://bhaveshbhavu.github.io/Colors/', 'https://bhaveshbhavu.github.io/RATHORE-BHAVESH/','https://bhaveshbhavu.github.io/-_-/','https://bhaveshbhavu.github.io/Hamster/','https://bhaveshbhavu.github.io/Free-g/','https://bhaveshbhavu.github.io/Download-/','https://github.com/');
 
 for (var i = 0; i < 1; i++) {
   
  var box = document.querySelector('.img_box');
   
 
- box.innerHTML += `<img src="${img[o]}" onclick="window.open('${img[o]}')"/>`;
+ box.innerHTML += `<img src="${img[o]}" onclick="window.open('${a[o]}')"/>`;
   
  
   }
