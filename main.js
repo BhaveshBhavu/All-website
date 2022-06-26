507df715dc19f51f8c187cf7d44df5c314538063
bhaveshabcd1234bhavu@gmail.com <bhaveshabcd1234bhavu@gmail.com>
2022-06-27 00:47:16 +0530

    physical

--- main.js
+++ main.js
@@ -2,18 +2,18 @@ console.log('Hello World!');
 
 
 
-for (var o = 0; o < 16; o++) {
+for (var o = 0; o < 17; o++) {
 
-var img = new Array('img/1.jpg', 'img/2.jpg','img/3.jpeg', 'img/4.png', 'img/5.jpg','img/6.jpg','img/7.jpeg', 'img/8.jpg','img/9.jpg', 'img/10.jpg', 'img/11.jpg','img/12.jpg','img/13.jpg','img/14.jpg','img/15.jpg','img/16.png');
+var img = new Array('1.jpg', '2.jpg','3.jpeg', '4.png', '5.jpg','6.jpg','7.jpeg', '8.jpg','9.jpg', '10.jpg', '11.jpg','12.jpg','13.jpg','14.jpg','15.jpg','16.png','17.jpg');
 
-var a = new Array('https://bhaveshbhavu.github.io/Doremon/', 'https://bhaveshbhavu.github.io/password-N/','https://bhaveshbhavu.github.io/LET-S-TRAVEL/', 'https://bhaveshbhavu.github.io/YOU-TUBE/', 'https://bhaveshbhavu.github.io/May-play/','https://bhaveshbhavu.github.io/food/','https://bhaveshbhavu.github.io/Loki/', /*'https://bhaveshbhavu.github.io/img/'*/'https://bhaveshbhavu.github.io/new-random-image/','https://bhaveshbhavu.github.io/HDJum.Com/', 'https://bhaveshbhavu.github.io/Colors/', 'https://bhaveshbhavu.github.io/RATHORE-BHAVESH/','https://bhaveshbhavu.github.io/-_-/','https://bhaveshbhavu.github.io/Hamster/','https://bhaveshbhavu.github.io/Free-g/','https://bhaveshbhavu.github.io/Download-/','https://github.com/');
+var a = new Array('https://bhaveshbhavu.github.io/Doremon/', 'https://bhaveshbhavu.github.io/password-N/','https://bhaveshbhavu.github.io/LET-S-TRAVEL/', 'https://bhaveshbhavu.github.io/YOU-TUBE/', 'https://bhaveshbhavu.github.io/May-play/','https://bhaveshbhavu.github.io/food/','https://bhaveshbhavu.github.io/Loki/','https://bhaveshbhavu.github.io/img/','https://bhaveshbhavu.github.io/HDJum.Com/', 'https://bhaveshbhavu.github.io/Colors/', 'https://bhaveshbhavu.github.io/RATHORE-BHAVESH/','https://bhaveshbhavu.github.io/-_-/','https://bhaveshbhavu.github.io/Hamster/','https://bhaveshbhavu.github.io/Free-g/','https://bhaveshbhavu.github.io/Download-/','https://github.com/','https://bhaveshbhavu.github.io/new-random-image/');
 
 for (var i = 0; i < 1; i++) {
   
  var box = document.querySelector('.img_box');
   
 
- box.innerHTML += `<img src="${img[o]}" onclick="window.open('${a[o]}')"/>`;
+ box.innerHTML += `<img src="img/${img[o]}" onclick="window.open('${a[o]}')"/>`;
   
  
   }
